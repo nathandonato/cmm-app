80119ddd9d4f35e6ce77439b026dcf127a5b3b89
Nathan Donato <nathanjdonato@gmail.com>
2019-11-15 11:14:43 -0500

    Add seeds for some test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,32 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database
-# with its default values.
-# The data can then be loaded with the rails db:seed command (or created
-# alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' },
-#                          { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+EMPLOYEES = [
+  ['Jane Janeson', 'jjaneson', 'jjaneson@foo.bar'],
+  ['Rick Rickson', 'rrickson', 'rrickson@foo.bar'],
+  ['Dev user', 'devuser', 'user@foo.bar']
+].freeze
+
+CUSTOMERS_AND_PROJECTS = {
+  "Pete's Delivery" => {
+    'Logo design' => ['Determine color scheme', 'Mock up logo']
+  },
+  "Veronica's Construction" => {
+    'Merch design' => ['Design t-shirt', 'Design hoodie']
+  }
+}.freeze
+
+EMPLOYEES.each do |_name, username, email|
+  next if User.find_by(username: username, email: email).present?
+
+  User.create!(username: username, email: email, password: 'testing123')
+end
+
+CUSTOMERS_AND_PROJECTS.each do |name, projects|
+  customer = Customer.find_or_create_by!(name: name)
+  projects.each do |p_description, tasks|
+    project = customer.projects.find_or_create_by!(description: p_description)
+    tasks.each do |t_description|
+      project.tasks.find_or_create_by!(description: t_description)
+    end
+  end
+end
